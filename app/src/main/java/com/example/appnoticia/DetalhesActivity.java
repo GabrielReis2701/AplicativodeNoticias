3d975142a209f1e3813e4d51026fba553969c92d
Gabriel <gabrielreis2701@gmail.com>
2022-08-04 08:58:45 -0300

    v.2

--- app/src/main/java/com/example/appnoticia/DetalhesActivity.java
+++ app/src/main/java/com/example/appnoticia/DetalhesActivity.java
@@ -3,12 +3,38 @@ package com.example.appnoticia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.appnoticia.Models.NewsHeadlines;
+import com.squareup.picasso.Picasso;
 
 public class DetalhesActivity extends AppCompatActivity {
+
+    NewsHeadlines headlines;
+    TextView txt_title, txt_author, txt_time, txt_detail, txt_content;
+    ImageView img_news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
+
+        txt_title = findViewById(R.id.text_detail_title);
+        txt_author = findViewById(R.id.text_detail_author);
+        txt_time = findViewById(R.id.text_detail_time);
+        txt_detail = findViewById(R.id.text_detail_detail);
+        txt_content = findViewById(R.id.text_detail_content);
+
+        headlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
+
+        txt_title.setText(headlines.getTitulo());
+        txt_author.setText(headlines.getAutor());
+        txt_time.setText(headlines.getPublicadoEm());
+        txt_detail.setText(headlines.getDescricao());
+        txt_content.setText(headlines.getContente());
+
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+
     }
 }
--- app/src/main/java/com/example/appnoticia/MainActivity.java
+++ app/src/main/java/com/example/appnoticia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appnoticia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,26 +9,70 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.se.omapi.SEService;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.appnoticia.Models.NewsApiResponse;
 import com.example.appnoticia.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener {
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener {
 
     RecyclerView recyclerView;
+    Button bt_1,bt_2,bt_3,bt_4,bt_5,bt_6,bt_7;
     CustomAdapter adapter;
     ProgressDialog dialog;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchView = findViewById(R.id.search_view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Procurando novos artigos de "+ query);
+                dialog.show();
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getNewsHeadlines(listener, "general", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         dialog = new ProgressDialog(this);
         dialog.setTitle("Buscando novos artigos...");
         dialog.show();
+
+        bt_1 = findViewById(R.id.btn_1);
+        bt_1.setOnClickListener(this);
+
+        bt_2 = findViewById(R.id.btn_2);
+        bt_2.setOnClickListener(this);
+
+        bt_3 = findViewById(R.id.btn_3);
+        bt_3.setOnClickListener(this);
+
+        bt_4 = findViewById(R.id.btn_4);
+        bt_4.setOnClickListener(this);
+
+        bt_5 = findViewById(R.id.btn_5);
+        bt_5.setOnClickListener(this);
+
+        bt_6 = findViewById(R.id.btn_6);
+        bt_6.setOnClickListener(this);
+
+        bt_7 = findViewById(R.id.btn_7);
+        bt_7.setOnClickListener(this);
 
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, "general", null);
@@ -37,13 +82,18 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
     private final OnFetchDataListener <NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
         @Override
         public void onfetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
-            dialog.dismiss();
+            if (list.isEmpty()){
+                Toast.makeText(MainActivity.this, "Dados não encontrados!!!", Toast.LENGTH_SHORT).show();
+            }else {
+                showNews(list);
+                dialog.dismiss();
+            }
+
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "Ocorreu um erro!!!!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -59,5 +109,15 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
     @Override
     public void OnNewsClicked(NewsHeadlines headlines) {
         startActivity(new Intent(MainActivity.this, DetalhesActivity.class).putExtra("data", headlines));
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String category = button.getText().toString();
+        dialog.setTitle("Buscando novos artigos da "+category);
+        dialog.show();
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
